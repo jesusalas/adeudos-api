73167de7e8cfd294fcd3cfdba00c728ad139ef39
jesus <jesusalas18@gmail.com>
2018-02-15 00:53:53 -0700

    Cambio diseño de tareas

--- static/js/vue/clases.js
+++ static/js/vue/clases.js
@@ -3,6 +3,23 @@ new Vue({
    delimiters: ['${', '}'],
    el:"#main",
    data:{
-      isActive: false
+      isActive: false,
+      tasks: [
+         {
+            description: 'Aprender Vue.js',
+            pending: true,
+            editing: false
+         },
+         {
+            description: 'Suscribirse a Styde.net',
+            pending: true,
+            editing: true
+         },
+         {
+            description: 'Grabar lección de Vue',
+            pending: false,
+            editing: false
+         }
+      ]
    }
 });
