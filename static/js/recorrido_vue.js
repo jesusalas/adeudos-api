66890ee45517e421330f388c858cfe0463299458
Jesús Salas <jesusalas18@gmail.com>
2018-02-22 16:37:16 -0700

    Cambio objeto vue para crear json de tablas

--- static/js/recorrido_vue.js
+++ static/js/recorrido_vue.js
@@ -1,95 +1,37 @@
-  
-  var urlRoutes= '/rutas';
-    new Vue({
-      delimiters: ['${', '}'],
-      el:"#routes",
-      created: function(){
-        this.getRoutes();
-      },
-      data: {
-          routes_list:[]
-      },
-      methods:{
-        getRoutes: function(){
-          this.$http.get(urlRoutes).then(function(response){
-              this.routes_list = response.data;
-          });
-        }
-      },
-    });
-
-  var urlCedis = "/cedis";
-    new Vue({
-      delimiters: ['${', '}'],
-      el:"#cedis",
-      created: function(){
-        this.getCedis();
-      },
-      data: {
-          cedis_list:[]
-      },
-      methods:{
-        getCedis: function(){
-          this.$http.get(urlCedis).then(function(response){
-              this.cedis_list = response.data;
-          });
-        }
-      },
-    });
-
-  var urlClients = "/clientes";
-    new Vue({
-      delimiters: ['${', '}'],
-      el:"#clientes",
-      created: function(){
-        this.getClients();
-      },
-      data: {
-          clients_list:[]
-      },
-      methods:{
-        getClients: function(){
-          this.$http.get(urlClients).then(function(response){
-              this.clients_list = response.data;
-          });
-        }
-      },
-    });
-
-  var urlVisits = "/visitas";
-    new Vue({
-      delimiters: ['${', '}'],
-      el:"#visitas",
-      created: function(){
-        this.getVisits();
-      },
-      data: {
-          visits_list:[]
-      },
-      methods:{
-        getVisits: function(){
-          this.$http.get(urlVisits).then(function(response){
-              this.visits_list = response.data;
-          });
-        }
-      },
-    });
-
-  var urlPointsGps = "/gps";
   new Vue({
     delimiters: ['${', '}'],
-    el:"#gps",
+    el:"#main",
     created: function(){
-      this.getPointsGps();
+      this.getData('rutas');
+      this.getData('cedis');
     },
     data: {
+        routes_list: [],
+        cedis_list: [],
+        clients_list: [],
+        visits_list: [],
         points_gps_list:[]
     },
     methods:{
-      getPointsGps: function(){
-        this.$http.get(urlPointsGps).then(function(response){
-            this.points_gps_list = response.data;
+      getData(type){
+        var urlData = "/" + type;
+        this.$http.get(urlData).then(function(response){
+            
+            switch(type) {
+                case "cedis": this.cedis_list = response.data;
+                    break;
+                case "rutas": this.routes_list = response.data;
+                    break;
+                case "visitas": this.visits_list = response.data;
+                    break;
+                case "clientes": this.clients_list = response.data;
+                    break;
+                case "gps": this.points_gps_list = response.data;
+                    break;
+                default: return false;
+            }
         });
       }
     },
   });
+    
